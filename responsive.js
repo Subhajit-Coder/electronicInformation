d2e6bc40c41a0cff523bdb7ae6139544943d3185
Subhajit-Coder <biswajitbouri60177@gmail.com>
2020-09-11 18:59:31 +0530

    electronicsInformationWebsite
    Visit our website here you can gather information about electronics and if you have any query please feel free to email at biswajitbouri60177@gmail.com

---
+++ responsive.js
@@ -0,0 +1,13 @@
+
+function search(){
+    var myText = document.getElementById("input1").value;
+    alert(" No results ");  
+}
+function cbutton(){
+    a = document.getElementById("name").value;
+    b = document.getElementById("email").value;
+    c = document.getElementById("password").value;
+    alert("name : "+a);
+    alert("email : "+b);
+    alert(a+" Thank You For Contact Us ");
+}
